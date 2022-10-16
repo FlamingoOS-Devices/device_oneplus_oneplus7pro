eeed160bb694a61f88669abdadccefc617b5c78e
Pranav <npv12@iitbbs.ac.in>
2022-10-16 17:09:43 +0530

    guacamole: drop fstab in favour of unified one in common tree
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

--- device.mk
+++ device.mk
@@ -22,10 +22,6 @@ TARGET_SCREEN_WIDTH := 1440
 PRODUCT_PACKAGES += \
     OnePlusCameraHelper
 
-# Device init scripts
-PRODUCT_PACKAGES += \
-    fstab.qcom
-
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
